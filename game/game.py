da7b7018806ed1b6b4e56eb16a594ef37b48aefd
barcern <bcernakova01@gmail.com>
2021-03-22 00:12:43 +0000

    Basic scoring added in

--- game/game.py
+++ game/game.py
@@ -26,7 +26,7 @@ background = pygame.image.load("background.jpg")
 
 # Player
 player_img = pygame.image.load("dog_walk.png")
-player_size = 30
+player_size = 70
 player_pos = [int(size[0]/2), size[1] - 300]
 
 # Enemy
@@ -37,11 +37,11 @@ collect_img = pygame.image.load("bone.png")
 collect_size = 30
 collect_pos = [random.randint(0, size[0]-collect_size), 0]
 collect_list = [collect_pos]
-collect_speed = 7
+collect_speed = 5
 
 def show_collect(collect_list):
     delay = random.random()
-    if len(collect_list) < 10 and delay < 0.06:
+    if len(collect_list) < 5 and delay < 0.06:
         collect_list.append([random.randint(0, size[0]-collect_size), 0])
 
 def display_collect(collect_list):
@@ -57,6 +57,27 @@ def new_collect_pos(collect_list):
             #score+=1
     #return score
 
+def multi_collide_collect(player_pos, collect_list, score):
+    for collect_pos in collect_list:
+        old_score = score
+        score = collide_collect(player_pos, collect_pos, score)
+        if old_score != score:
+            collect_list.remove(collect_pos)
+    return score
+
+def collide_collect(player_pos, collect_pos, score):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+    c_x = collect_pos[0]
+    c_y = collect_pos[1]
+    if (c_x>=p_x and c_x<(p_x+player_size)) or (p_x>=c_x and p_x<(c_x+collect_size)):
+        if(c_y>=p_y and c_y<(p_y+player_size)) or (p_y>=c_y and p_y<(c_y+collect_size)):
+            score += 1
+            
+            return score
+    return score
+
+myFont=pygame.font.SysFont("calibri",35, bold=True, italic=True)
 flag = True
 score = 0
 clock = pygame.time.Clock()
@@ -77,10 +98,14 @@ while flag:
     #pygame.draw.rect(screen, red, [55, 200, 100, 70],0)
     show_collect(collect_list)
     new_collect_pos(collect_list)
+    score = multi_collide_collect(player_pos, collect_list, score)
+    text="Your Score: "+str(score)
+    label=myFont.render(text, 1, (204, 255, 51))
+    screen.blit(label,(int(size[0]-250),0))
     display_collect(collect_list)
     screen.blit(player_img,(player_pos[0],player_pos[1]))
     #pygame.display.flip()
-    clock.tick(20)
+    clock.tick(30)
     pygame.display.update()
             
 pygame.quit()
